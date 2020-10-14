cf66084a65ea04ed7d12481d725f38b57be1acb7
andre <a.slokker@student.han.nl>
2020-10-14 16:03:36 +0200

    removed old picture

--- prototypes/cue_ball_detection/config.py
+++ prototypes/cue_ball_detection/config.py
@@ -22,17 +22,6 @@ pictures = {
         "left_bottom_corner_table": (705, 873)
     },
     "picture_3" : {
-        "location" : "used_pictures/setup.jpg", # background color and white ball colors are not perfect, may need to be adjusted.
-        "background_color_hsv_min" : (101, 160, 104),
-        "background_color_hsv_max" : (104, 201, 161),
-        "white_ball_color_hsv_min" : (37, 40, 175),
-        "white_ball_color_hsv_max" : (53, 81, 243),
-        "left_top_corner_table" : (345, 67),
-        "right_top_corner_table" : (1688, 123),
-        "right_bottom_corner_table" : (1662, 783),
-        "left_bottom_corner_table": (360, 757)
-    },
-    "picture_4" : {
         "location" : "used_pictures/setup5_3.jpg",
         "background_color_hsv_min" : (107, 0, 108),
         "background_color_hsv_max" : (115, 255, 151),
