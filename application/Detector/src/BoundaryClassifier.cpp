21dfd575b23a5494c14bfe590e15935b16e46904
TomSievers <tom.sievers13@gmail.com>
2020-12-02 14:58:33 +0100

    remove possibility for nullptr cast

--- application/Detector/src/BoundaryClassifier.cpp
+++ application/Detector/src/BoundaryClassifier.cpp
@@ -15,28 +15,33 @@ namespace Detector
 
     std::shared_ptr<void> BoundaryClassifier::process(cv::Mat& img, std::shared_ptr<void> data)
     {
-        BoundaryClassifierData boundData = *std::static_pointer_cast<BoundaryClassifierData>(data);
-        int offset = boundData.imgWidth/50;
-        std::shared_ptr<Boundary> boundary = std::make_shared<Boundary>();
+        if(data != nullptr)
+        {
+            BoundaryClassifierData boundData = *std::static_pointer_cast<BoundaryClassifierData>(data);
+            int offset = boundData.imgWidth/50;
+            std::shared_ptr<Boundary> boundary = std::make_shared<Boundary>();
 
-        boundary->corners = {
-            cv::Point(offset, offset), 
-            cv::Point(boundData.imgWidth-offset, offset), 
-            cv::Point(boundData.imgWidth-offset, boundData.imgHeight-offset), 
-            cv::Point(offset, boundData.imgHeight-offset)
-        };
+            boundary->corners = {
+                cv::Point(offset, offset), 
+                cv::Point(boundData.imgWidth-offset, offset), 
+                cv::Point(boundData.imgWidth-offset, boundData.imgHeight-offset), 
+                cv::Point(offset, boundData.imgHeight-offset)
+            };
 
-        boundary->pocketsLoc = {
-            cv::Point(offset, offset), 
-            cv::Point(boundData.imgWidth/2, 0),
-            cv::Point(boundData.imgWidth-offset, offset), 
-            cv::Point(boundData.imgWidth-offset, boundData.imgHeight-offset), 
-            cv::Point(boundData.imgWidth/2, boundData.imgHeight),
-            cv::Point(offset, boundData.imgHeight-offset)
-        };
+            boundary->pocketsLoc = {
+                cv::Point(offset, offset), 
+                cv::Point(boundData.imgWidth/2, 0),
+                cv::Point(boundData.imgWidth-offset, offset), 
+                cv::Point(boundData.imgWidth-offset, boundData.imgHeight-offset), 
+                cv::Point(boundData.imgWidth/2, boundData.imgHeight),
+                cv::Point(offset, boundData.imgHeight-offset)
+            };
 
-        boundary->pocketRad = boundData.imgWidth/50;
+            boundary->pocketRad = boundData.imgWidth/50;
+            
+            return boundary;
+        }
+        return nullptr;
         
-        return boundary;
     }
 } // namespace Detector
