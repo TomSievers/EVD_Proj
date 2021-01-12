098a01a07816a1d09ee7c433812fef6c276ce49e
Martijn Visser <m.visser.2@hotmail.com>
2021-01-12 11:02:56 +0100

    Updated diagrams

--- application/Detector/src/ChangeClassification.cpp
+++ application/Detector/src/ChangeClassification.cpp
@@ -1,6 +1,8 @@
 #include "include/Change/ChangeClassification.hpp"
 #include <iostream>
 
+#define CHANGE_THRESHOLD 0
+
 namespace Detector
 {
     ChangeClassification::ChangeClassification()
@@ -16,7 +18,7 @@ namespace Detector
     std::shared_ptr<void> ChangeClassification::process(cv::Mat& img, std::shared_ptr<void> data)
     {
         std::shared_ptr<ChangeObject> objectPtr = std::static_pointer_cast<ChangeObject>(data);
-        objectPtr->moving = objectPtr->nonZero > 0;
+        objectPtr->moving = objectPtr->nonZero > CHANGE_THRESHOLD;
         return nullptr;
     }
 
